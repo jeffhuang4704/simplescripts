dafcc7ecaf50f81b1b086aeb5d96ff3a778508f9
ChihJen Huang <jeffhuang4704@gmail.com>
2025-02-06 11:19:15 -0800

    backup

--- gotalent.sh
+++ gotalent.sh
@@ -2,19 +2,24 @@
 
 # generate for github cli
 cat > ~/0_script_github.sh <<EOF
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) 
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings 
-        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg 
-        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null 
+#!/bin/bash
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y))
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
 	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg 
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
-	&& sudo apt update 
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	&& sudo apt update
 	&& sudo apt install gh -y
 EOF
+
 chmod +x ~/0_script_github.sh
 
 # generate for gotalent
 cat > ~/1_script_gotalent.sh <<EOF
+#!/bin/bash
+
 gh auth login
 
 git clone https://github.com/jeffhuang4704/GoTalent.git
@@ -30,6 +35,8 @@ chmod +x ~/1_script_gotalent.sh
 
 # generate for gocheck
 cat > ~/2_script_gocheck.sh <<EOF
+#!/bin/bash
+
 gh auth login
 
 git clone https://github.com/jeffhuang4704/GPortal-Sign-Off.git
