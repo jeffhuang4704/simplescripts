486be4a4d7753bfd409b47b4edb27b3bf40591df
ChihJen Huang <jeffhuang4704@gmail.com>
2025-02-06 11:31:48 -0800

    backup

--- install_gh.sh
+++ install_gh.sh
@@ -1,4 +1,4 @@
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y))
+(sudo apt update && sudo apt-get install wget -y) \
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
         && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
         && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
