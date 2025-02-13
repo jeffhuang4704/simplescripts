7dfb465512b9bf569f17193438f3844ade9e7954
ChihJen Huang <jeffhuang4704@gmail.com>
2025-02-13 14:43:10 -0800

    update

--- install_nv.sh
+++ install_nv.sh
@@ -2,7 +2,10 @@ helm repo add neuvector https://neuvector.github.io/neuvector-helm/
 helm search repo neuvector/core
 
 kubectl create namespace neuvector
-helm install neuvector --namespace neuvector --create-namespace neuvector/core
+helm install neuvector --namespace neuvector --create-namespace \
+  --set controller.env[0].name="CTRL_PATH_DEBUG" \
+  --set controller.env[0].value="1" \
+  neuvector/core
 
 echo 'wait for a while...'
 sleep 10
@@ -11,6 +14,10 @@ kubectl patch svc neuvector-service-webui -n neuvector --type='merge' -p '{"spec
 # Get the NodePort number of the service
 NODE_PORT=$(kubectl get svc neuvector-service-webui -n neuvector -o jsonpath='{.spec.ports[0].nodePort}')
   
+# Get playground id
+PLAYGROUND_ID=$(labctl playground list | awk 'NR==2 {print $1}')
 
 echo '🔴 go back to WSL to do port-forwarding'
-echo "labctl port-forward -m cplane-01 {id} -L $NODE_PORT:$NODE_PORT"
+echo "then open your browser and go to https://localhost:$NODE_PORT"
+echo ''
+echo "labctl port-forward -m cplane-01 $PLAYGROUND_ID -L $NODE_PORT:$NODE_PORT"
